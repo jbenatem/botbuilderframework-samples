c5a3890171e548bf12238e589432fcad6cfb4afb
Juan Domingo Benate Mendoza <jbenatem@everis.com>
2020-10-13 18:45:54 -0500

    Primer commit, falta registrar todos los mensajes enviados, solo guarda el ultimo mensaje enviado

---
+++ SaveDataInJsonBot/Model/DataModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaveDataInJsonBot.Model
+{
+    public class MensajeEnviado
+    {
+        public string Emisor { get; set; }
+        public string Receptor { get; set; }
+        public string Mensaje { get; set; }
+        public string Timestamp { get; set; }
+    }
+    public class HistorialMensajes
+    {
+        public List<MensajeEnviado> MensajesEnviados { get; set; }
+    }
+}
